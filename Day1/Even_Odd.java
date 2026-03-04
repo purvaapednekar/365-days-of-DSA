040d1b0e8d7a40d3787167479c5a367af82f16ae
Purvaa Pednekar <purvaapednekarii@gmail.com>
2026-03-04 07:52:31 +0530

    Day1 -> Even Odd problem

---
+++ Day1/Even_Odd.java
@@ -0,0 +1,17 @@
+package assignment.one;
+
+import java.util.Scanner;
+
+public class Even_Odd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
