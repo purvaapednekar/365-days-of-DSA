e160f0fac892d6597915c7e55a959c5a6fb58c52
Purvaa Pednekar <purvaapednekarii@gmail.com>
2026-03-05 18:15:55 +0530

    Area of Rhombus

---
+++ Day2/Perimeter/Rhombus.java
@@ -0,0 +1,15 @@
+package assignment.two.perimeter;
+
+import java.util.Scanner;
+
+public class Rhombus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter side : ");
+        int side = sc.nextInt();
+
+        int perimeter = 4 * side;
+        System.out.println("Perimeter of rhombus : " + perimeter);
+    }
+}
