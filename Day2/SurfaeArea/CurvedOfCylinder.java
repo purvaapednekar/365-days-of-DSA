fabd047b0c4f90f8c5981471f04b12c1d7bdecc0
Purvaa Pednekar <purvaapednekarii@gmail.com>
2026-03-06 11:48:03 +0530

    Curved Surface Area of Cylinder

---
+++ Day2/SurfaeArea/CurvedOfCylinder.java
@@ -0,0 +1,19 @@
+package assignment.two.surfaceArea;
+
+import java.util.Scanner;
+
+public class CurvedOfCylinder {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter height : ");
+        int height = sc.nextInt();
+
+        System.out.print("Enter radius : ");
+        int radius = sc.nextInt();
+
+        float CSA = 2 * 3.14f * radius * height;
+        System.out.println("Curved surface area of cylinder : " + CSA);
+
+    }
+}
