03589b843c3426b9c0623d5edd02f25ff2fe4ed1
Purvaa Pednekar <purvaapednekarii@gmail.com>
2026-03-09 00:44:00 +0530

    Create Factorial.java

---
+++ Day5/Factorial.java
@@ -0,0 +1,21 @@
+package assignment.two.others;
+//Factorial of number
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number : ");
+        int num = sc.nextInt();
+
+        int fact = 1;
+
+        for (int i = num; i >= 1 ; i--) {
+            fact = fact * i;
+        }
+        
+        System.out.println("Factorial of number : " + fact);
+    }
+}
