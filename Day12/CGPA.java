c8b62460b4c830af92d6245a18764bbb5451e58b
Purvaa Pednekar <purvaapednekarii@gmail.com>
2026-03-27 00:01:46 +0530

    Create CGPA.java

---
+++ Day12/CGPA.java
@@ -0,0 +1,24 @@
+package assignment.two.intermediate;
+
+import java.util.Scanner;
+
+public class CGPA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of subjects : ");
+        int n = sc.nextInt();
+        
+        double sum = 0;
+
+        for (int i = 1; i <= n ; i++) {
+            System.out.println("Enter grade point of : " + "sub" + i);
+            double grade = sc.nextDouble();
+            sum += 0;
+            
+        }
+        
+        double cgpa = sum / n;
+        System.out.println(cgpa);
+    }
+}
