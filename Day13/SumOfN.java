c482db2b0427f0d2629938a59c3f5b9711bba732
Purvaa Pednekar <purvaapednekarii@gmail.com>
2026-03-28 19:49:55 +0530

    Create SumOfN.java

---
+++ Day13/SumOfN.java
@@ -0,0 +1,20 @@
+package assignment.two.intermediate;
+
+import java.util.Scanner;
+
+public class SumOfN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("The sum of n is " + sum);
+    }
+}
