3e2f25b5635124023d307cd277728ab26ab20347
Purvaa Pednekar <purvaapednekarii@gmail.com>
2026-04-04 10:28:10 +0530

    Create HCF.java

---
+++ Day18/HCF.java
@@ -0,0 +1,22 @@
+package assignment.two.intermediate;
+
+import java.util.Scanner;
+
+public class HCF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter b : ");
+        int b = sc.nextInt();
+
+        while(b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        System.out.println("HCF = " + a);
+    }
+}
